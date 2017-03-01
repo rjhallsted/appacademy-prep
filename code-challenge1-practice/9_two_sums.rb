7f2397e7d86d1477401401ff3d96f3b9cb4ff54b
rjhallsted <rjhallsted@gmail.com>
2017-03-01 14:32:49 -0700

    Solved 9:Two sums.

---
+++ code-challenge1-practice/9_two_sums.rb
@@ -0,0 +1,28 @@
+# Write a method that takes an array of numbers. If a pair of numbers
+# in the array sums to zero, return the positions of those two numbers.
+# If no pair of numbers sums to zero, return `nil`.
+#
+# Difficulty: medium.
+
+def two_sum(nums)
+  nums.each_with_index do |number, index|
+    slice_length = nums.size - index - 1
+    if nums[index+1, slice_length].any? { |x| x == nums[index] * -1! }
+      return [index, nums.find_index(number * -1)]
+    end
+  end
+  nil
+end
+
+# These are tests to check that your code is working. After writing
+# your solution, they should all print true.
+
+puts("\nTests for #two_sum")
+puts("===============================================")
+    puts(
+      'two_sum([1, 3, 5, -3]) == [1, 3]: ' + (two_sum([1, 3, 5, -3]) == [1, 3]).to_s
+    )
+    puts(
+      'two_sum([1, 3, 5]) == nil: ' + (two_sum([1, 3, 5]) == nil).to_s
+    )
+puts("===============================================")
